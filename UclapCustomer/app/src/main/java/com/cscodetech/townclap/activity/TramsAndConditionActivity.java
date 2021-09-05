bc07f29df87971faa803a5d1d8de52d3ed99c807
kalkaprasad <52508495+kalkaprasad@users.noreply.github.com>
2021-09-05 11:20:58 +0530

    V1 UclapCustomer

---
+++ UclapCustomer/app/src/main/java/com/cscodetech/townclap/activity/TramsAndConditionActivity.java
@@ -0,0 +1,33 @@
+package com.cscodetech.townclap.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+
+import com.cscodetech.townclap.R;
+import com.cscodetech.townclap.utils.SessionManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.cscodetech.townclap.utils.SessionManager.terms;
+
+public class TramsAndConditionActivity extends BasicActivity {
+    @BindView(R.id.txt_privacy)
+    TextView txtPrivacy;
+    SessionManager sessionManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trams_and_condition);
+        ButterKnife.bind(this);
+        sessionManager = new SessionManager(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            txtPrivacy.setText(Html.fromHtml(sessionManager.getStringData(terms), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            txtPrivacy.setText(Html.fromHtml(sessionManager.getStringData(terms)));
+        }
+    }
+}
